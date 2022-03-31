b96027e72c7cf3582265f87483dc6eb6b39bafc8
Cesar <cesareli17188@gmail.com>
2022-03-31 16:02:37 -0500

    angular router y ionic storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,56 +1,10 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  slideOps = {
-    initialSlide: 0,
-    slidesPerView: 1,
-    centeredSlides: true,
-    speed: 400,
-  };
-
-  slides = [
-    {
-      imageSrc: 'assets/img/logo.png',
-      imageAlt: 'Platzi Music Logo',
-      title: 'Escucha tu música',
-      subtitle: 'EN CUALQUIER LUGAR',
-      description:
-        'Los mejores álbunes, las mejores canciones. Escucha y comparte en cualquier momento, a todas horas.',
-      icon: 'play',
-    },
-    {
-      imageSrc: 'assets/img/logo.png',
-      imageAlt: 'Platzi Music Logo',
-      title: 'Disfruta de nuestro reproductor',
-      subtitle: 'DE VIDEOS INCREÍBLES',
-      description:
-        'Entra la modo video de nuestro reproductor y obtén acceso a clips. documentales y making offs increíbles de tu artista favorito',
-      icon: 'videocam',
-    },
-    {
-      imageSrc: 'assets/img/logo.png',
-      imageAlt: 'Platzi Music Logo',
-      title: 'Accede al exclusivo',
-      subtitle: 'MODO DEPORTE',
-      description:
-        'Crea una playlist basada en tu actividad física. Ten reportes y acceso a lo que necesites, integrado con GPS!',
-      icon: 'bicycle',
-    },
-    {
-      imageSrc: 'assets/img/logo.png',
-      imageAlt: 'Platzi Music Logo',
-      title: 'Busca nuevas canciones',
-      subtitle: 'EN LA RADIO DE TU AUTOR FAVORITO',
-      description:
-        'Mira las playlist que se generan con los ritnos y generos parecidos a los de autor favorito',
-      icon: 'albums',
-    },
-  ];
 
   constructor() {}
 }
---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Storage } from '@ionic/storage-angular';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+
+  slideOps = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    centeredSlides: true,
+    speed: 400,
+  };
+
+  slides = [
+    {
+      imageSrc: 'assets/img/logo.png',
+      imageAlt: 'Platzi Music Logo',
+      title: 'Escucha tu música',
+      subtitle: 'EN CUALQUIER LUGAR',
+      description:
+        'Los mejores álbunes, las mejores canciones. Escucha y comparte en cualquier momento, a todas horas.',
+      icon: 'play',
+    },
+    {
+      imageSrc: 'assets/img/logo.png',
+      imageAlt: 'Platzi Music Logo',
+      title: 'Disfruta de nuestro reproductor',
+      subtitle: 'DE VIDEOS INCREÍBLES',
+      description:
+        'Entra la modo video de nuestro reproductor y obtén acceso a clips. documentales y making offs increíbles de tu artista favorito',
+      icon: 'videocam',
+    },
+    {
+      imageSrc: 'assets/img/logo.png',
+      imageAlt: 'Platzi Music Logo',
+      title: 'Accede al exclusivo',
+      subtitle: 'MODO DEPORTE',
+      description:
+        'Crea una playlist basada en tu actividad física. Ten reportes y acceso a lo que necesites, integrado con GPS!',
+      icon: 'bicycle',
+    },
+    {
+      imageSrc: 'assets/img/logo.png',
+      imageAlt: 'Platzi Music Logo',
+      title: 'Busca nuevas canciones',
+      subtitle: 'EN LA RADIO DE TU AUTOR FAVORITO',
+      description:
+        'Mira las playlist que se generan con los ritnos y generos parecidos a los de autor favorito',
+      icon: 'albums',
+    },
+  ];
+
+  constructor(
+    private router: Router,
+    private storage: Storage,
+    ) { }
+
+  ngOnInit() {
+  }
+
+  finish() {
+    this.storage.create();
+    this.storage.set('isIntroShowed', true);
+    this.router.navigateByUrl('/home');
+  }
+
+}
